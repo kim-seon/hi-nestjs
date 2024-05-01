3ac1ecebecdf53733215d3c00fbd416f0e8de4b9
kim-seon <7bhz_@naver.com>
2024-05-01 15:59:46 +0900

    add Pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   // function 이름은 아무거나 해도 됨
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
